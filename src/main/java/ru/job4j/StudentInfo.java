9920e0fbe00be5d632ab2e969a85cd737ce0e7db
Madiyar Yp <madimadiar96@gmail.com>
2023-06-27 14:59:18 +0600

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Madiyar Yerezhep");
+        System.out.println("29.08.2023");
+    }
+}
