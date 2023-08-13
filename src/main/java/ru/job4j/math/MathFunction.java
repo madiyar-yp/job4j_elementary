20bb75599487e24bd51fb3e0ab3d19d66ac7e55a
Madiyar Yp <madimadiar96@gmail.com>
2023-08-13 15:39:37 +0600

    1.1. Что такое import [#504773 #436321]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -9,11 +9,11 @@ public class MathFunction {
         return first * second;
     }
 
-    public static double difference(double first, double second) {
-        return first - second;
-    }
-
     public static double division(double first, double second) {
         return first / second;
+    }
+
+    public static double difference(double first, double second) {
+        return first - second;
     }
 }
