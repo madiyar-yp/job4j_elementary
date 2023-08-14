b586a345e7acf4480b32f94b419ecf923b44cd0b
Madiyar Yp <madimadiar96@gmail.com>
2023-08-14 18:52:13 +0600

    4.1.2. Глупый бот [#31323 #436749]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -3,15 +3,11 @@ package ru.job4j.condition;
 public class DummyBot {
     public static String answer(String question) {
         String rsl = "I don't know. Please, ask another question.";
-
         if ("Hi, Bot.".equals(question)) {
-            rsl = "Hi, SmartAss.";
-            return rsl;
+            return "Hi, SmartAss.";
         } else if ("Bye.".equals(question)) {
-            rsl = "See you later.";
-            return rsl;
+            return "See you later.";
         }
-
         return rsl;
     }
 
