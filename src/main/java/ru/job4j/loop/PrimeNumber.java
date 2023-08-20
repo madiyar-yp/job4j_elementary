2b0041b0d336aa2a4bf6f3c6103150e4ef305999
Madiyar Yp <madimadiar96@gmail.com>
2023-08-20 09:04:21 +0600

    5.6. Простые числа [#156314]

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -5,7 +5,7 @@ public class PrimeNumber {
         int count = 0;
         for (int i = 2; i <= finish; i++) {
             if (CheckPrimeNumber.check(i)) {
-                count += 1;
+                count++;
             }
         }
         return count;
