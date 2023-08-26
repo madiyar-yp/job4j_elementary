0ea67ed78425b6daad102af55fcde67b79f3cff4
Madiyar Yp <madimadiar96@gmail.com>
2023-08-26 10:56:55 +0600

    6.7.4. Выигрышные комбинации в сокобан [#53859]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -30,4 +30,15 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
 }
