fb838e26d3834bed5bad7911a619fdfe80d09744
Madiyar Yp <madimadiar96@gmail.com>
2023-08-27 14:04:30 +0600

    6.8. Метод двух указателей [#225]

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -15,10 +15,4 @@ public class TwoNumberSum {
         }
         return new int[0];
     }
-
-    public static void main(String[] args) {
-        int[] result = TwoNumberSum.getIndexes(new int[] {1, 3, 4, 8, 9 }, 4);
-        System.out.println(result[0]);
-        System.out.println(result[1]);
-    }
 }
