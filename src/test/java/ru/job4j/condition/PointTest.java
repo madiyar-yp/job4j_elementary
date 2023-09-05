097dc62b16429519c887a8302562dc9d03d1f5f5
Madiyar Yp <madimadiar96@gmail.com>
2023-09-05 08:52:14 +0600

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -4,37 +4,30 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class PointTest {
-
     @Test
     void whenX10Y10X22Y20Then2dot0() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double rsl = a.distance(b);
         double expected = 2.0;
-        double rsl = Point.distance(x1, y1, x2, y2);
         assertThat(rsl).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenX12Y1minus7X29Y26Then14dot76() {
-        int x1 = 2;
-        int y1 = -7;
-        int x2 = 9;
-        int y2 = 6;
+        Point a = new Point(2, -7);
+        Point b = new Point(9, 6);
+        double rsl = a.distance(b);
         double expected = 14.76;
-        double rsl = Point.distance(x1, y1, x2, y2);
         assertThat(rsl).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenX12Y11X25Y23Then3dot60() {
-        int x1 = 2;
-        int y1 = 1;
-        int x2 = 5;
-        int y2 = 3;
+        Point a = new Point(2, 1);
+        Point b = new Point(5, 3);
+        double rsl = a.distance(b);
         double expected = 3.60;
-        double rsl = Point.distance(x1, y1, x2, y2);
         assertThat(rsl).isEqualTo(expected, withPrecision(0.01));
     }
 }
